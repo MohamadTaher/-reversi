e840eebcf68b2dc520988d53c21549b8b099391f
MohamadTaher <67930481+MohamadTaher@users.noreply.github.com>
2020-07-28 19:22:01 -0600

    Update my_second_attempt(static).py

--- my_second_attempt(static).py
+++ my_second_attempt(static).py
@@ -1,57 +1,17 @@
-"""
-    1.1: check if there is a blue player horizontally virtually or diagonally.
-
-    1.2: horizontally by checking the same row,
-     vertically by checking the same column,
-     diagonally by checking rows and columns at the same time.
-
-    1.3: if yes than check its right/left when horizontally,
-     up/down    when vertically,
-     and two directions when diagonally.
-
-    1.4: if any of the valid moves is true, than change 0 and 1||2 to number 5
-"""
 import numpy as np
 #                  0 1 2 3 4 5 6 7 8 9
 board = np.array([[6,6,6,6,6,6,6,6,6,6], #0
                   [6,0,0,0,0,0,0,0,0,6], #1
                   [6,0,0,0,0,0,0,0,0,6], #2
                   [6,0,0,0,0,0,0,0,0,6], #3
-                  [6,0,0,0,1,2,0,0,0,6], #4
-                  [6,0,0,0,2,1,0,0,0,6], #5
+                  [6,0,0,0,1,0,0,0,0,6], #4
+                  [6,2,0,1,0,2,0,0,0,6], #5
                   [6,0,0,0,0,0,0,0,0,6], #6
                   [6,0,0,0,0,0,0,0,0,6], #7
                   [6,0,0,0,0,0,0,0,0,6], #8
                   [6,6,6,6,6,6,6,6,6,6]])#9
 
-
-existedRed = []
-existedBlue = []
-for indexs, items in np.ndenumerate(board):
-    if items == 1:
-        existedRed.append(indexs)
-    if items == 2:
-        existedBlue.append(indexs)
-
-    
-def checkRedHor(row):
-    if np.any(board[row, :] == 1):
-        return np.argwhere(board[row, :] == 1)
-def checkRedVer(col):
-    return np.argwhere(board[:, col] == 1)
-def checkBlueHor(row):
-    return np.argwhere(board[row, :] ==2)
-def checkBlueVer(col):
-    return np.argwhere(board[:, col] == 2)
-
-for i in range(10):
-    if checkRedHor(i) != None:
-        print(checkRedHor(i))
-
-#this function is currently is not used.
 def validMove(row,col):
-    rightEmpty, leftEmpty, upEmpty, downEmpty = False, False, False, False
-
     def checkRight(row,col):
         cRight = row + 1
         checkRight = board[row][cRight]
@@ -69,15 +29,70 @@ def validMove(row,col):
         checkDown = board[row][cDown]
         return checkDown
 
-    if checkRight(row,col) == 0:
-        rightEmpty = True
-    if checkLeft(row,col) == 0:
-        leftEmpty = True
-    if checkUp(row,col) == 0:
-        upEmpty = True 
-    if checkDown(row,col) == 0:
+
+existedRed, existedBlue = [], []
+for indexs, items in np.ndenumerate(board):
+    if items == 1:
+        existedRed.append(indexs)
+    if items == 2:
+        existedBlue.append(indexs)
+
+   
+def checkRedHor(row):
+    if np.any(board[row, :] == 1):
+        return np.argwhere(board[row, :] == 1)
+    else:
+        return False  
+def checkRedVer(col):
+    if np.any(board[:, col] == 1):
+        return np.argwhere(board[:, col] == 1)
+    else:
+        return False
+def checkBlueHor(row):
+    if np.any(board[row, :] == 2):
+        return np.argwhere(board[row, :] == 2)
+    else:
+        return False  
+def checkBlueVer(col):
+    if np.any(board[:, col] == 2):
+        return np.argwhere(board[:, col] == 2)
+    else:
         downEmpty = True
+        return False
 
 
+print('red stones:', existedRed, "\n", 'blue stones:', existedBlue, '\n')
+for i in range(len(existedRed)):
+    h = existedRed[i][0]
+    if np.any(checkBlueHor(h) != False):
+        if np.any(existedRed[i][1] > checkBlueHor(h)):
+            print(checkBlueHor(h), 'is the blue stone location at the row', h, '\n the blue is also on the left of', existedRed[i][1])
+        if np.any(existedRed[i][1] < checkBlueHor(h)):#on the right
+            print(checkBlueHor(h), 'is the blue stone location at the row', h, '\n the blue is also on the right of', existedRed[i][1])    
         
+    else:
+        print('there is no blue pleyer at row', h, '\n')    
+    
+
+
+
+redTurn = True
+blueTurn = False
+
+if redTurn == True:
+    blueTurn = True
+    redTurn = False
+else:
+    blueTurn = True
+    redTurn = False
+
+
+
+
+
+
+
+
+
+
         
