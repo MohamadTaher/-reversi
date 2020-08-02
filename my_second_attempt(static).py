6d980ee055666e2ccd0231992aabc2ebf42e9a36
MohamadTaher <67930481+MohamadTaher@users.noreply.github.com>
2020-08-01 21:17:43 -0600

    Update my_second_attempt(static).py

--- my_second_attempt(static).py
+++ my_second_attempt(static).py
@@ -1,27 +1,97 @@
 import numpy as np
+import turtle
+import time
+import math
+wn = turtle.Screen()
+wn.bgcolor("grey")
+wn.setup(550,550)
+
+s = turtle.Turtle()
+s.pensize(1)
+s.color('black')
+s.speed(0)
+
+#drawing shapes
+def circle(f):
+    for i in range(18):
+        s.forward(f)
+        s.left(20)
+def square (x,y):
+    s.up()
+    s.goto(x,y)
+    s.down()
+    for m in range(4):
+        s.forward(65)
+        s.right(90)
+def drawboard():
+    def drawingboxs(x):
+        for y in range (8):
+            square(x, 260-(y *65))   
+    for i in range(8):
+        drawingboxs(-260 + (i*65))
+def possibleMoveCircle(y, x):
+    s.up()
+    s.color('black', 'green')
+    s.begin_fill()
+    s.goto(x+16, y-9)
+    s.setheading(-135)
+    s.down()
+    circle(10)
+    s.up()
+    s.setheading(0)
+    s.end_fill()
+def redplayer (y, x):
+    s.up()
+    s.color('black', 'red')
+    s.begin_fill()
+    s.goto(x+16, y-9)
+    s.setheading(-135)
+    s.down()
+    circle(10)
+    s.up()
+    s.setheading(0)
+    s.end_fill()
+def blueplayer (y, x):
+    s.up()
+    s.color('black', 'blue')
+    s.begin_fill()
+    s.goto(x+16, y-9)
+    s.setheading(-135)
+    s.down()
+    circle(10)
+    s.up()
+    s.setheading(0)
+    s.end_fill()
+
+
+
 #                  0 1 2 3 4 5 6 7 8 9
 board = np.array([[6,6,6,6,6,6,6,6,6,6], #0
                   [6,0,0,0,0,0,0,0,0,6], #1
                   [6,0,0,0,0,0,0,0,0,6], #2
                   [6,0,0,0,0,0,0,0,0,6], #3
                   [6,0,0,0,0,0,0,0,0,6], #4
-                  [6,0,0,0,0,0,0,0,0,6], #5
+                  [6,0,-1,0,0,1,0,0,0,6], #5
                   [6,0,0,0,0,0,0,0,0,6], #6
-                  [6,0,0,0,0,0,1,2,0,6], #7
+                  [6,0,0,0,0,0,-1,1,0,6], #7
                   [6,0,0,0,0,0,0,0,0,6], #8
                   [6,6,6,6,6,6,6,6,6,6]])#9
 
 redTurn, blueTurn = True, False
-existedRed, existedBlue = [], []
 
+rows, columns = [], []
+for i in range(10):
+    rows.append(325-(i*65))
+    columns.append(-325 + (i*65))
+
+existedRed, existedBlue, existedGreen = [], [], []
 for indexs, items in np.ndenumerate(board):
     if items == 1:
         existedRed.append(indexs)
-    if items == 2:
+    if items == -1:
         existedBlue.append(indexs)
-
-existedStones = existedRed + existedBlue
-
+    if items == 5:
+        existedGreen.append(indexs)
 
 def checkRight(row,col):
     cRight = col + 1
@@ -39,62 +109,46 @@ def checkDown(row,col):
     cDown = row - 1
     checkDown = board[cDown][col]
     return checkDown
-
-
-
-if checkLeft(7,7) == 2 or 1:
-    print('hi')
    
-def checkRedHor(row):
-    if np.any(board[row, :] == 1):
-        return np.argwhere(board[row, :] == 1)
-    else:
-        return False  
-def checkRedVer(col):
-    if np.any(board[:, col] == 1):
-        return np.argwhere(board[:, col] == 1)
-    else:
-        return False
-def checkBlueHor(row):
-    if np.any(board[row, :] == 2):
-        return np.argwhere(board[row, :] == 2)
-    else:
-        return False  
-def checkBlueVer(col):
-    if np.any(board[:, col] == 2):
-        return np.argwhere(board[:, col] == 2)
-    else:
-        downEmpty = True
-        return False
 
-
-print('red stones:', existedRed, "\n", 'blue stones:', existedBlue, '\n', existedStones)
+print('red stones:', existedRed, 'blue stones:', existedBlue)
 while redTurn:
     for i in range(len(existedRed)):
-        rowOfRed = existedRed[i][0]
-        colOfRed = existedRed[i][1]
-        if checkLeft(rowOfRed,colOfRed) == 2:
-            print('on the left')
-            break
-        else:
-            print('not on the left')
-            break
+        row = existedRed[i][0]
+        col = existedRed[i][1]
+        left_moves = False
+        bluefound = False
+        left_moves = 0
+        for k in range(8): 
+            if checkLeft(row, col - k) == -1:
+                ltimes = ltimes +1
+                bluefound = True
+                print(row, col, bluefound)
+            if bluefound ==True:
+                if checkLeft(row, col - k ) == 0:
+                    board[row, col - k - 1] = 5
+                    # possibleMoveCircle(rows[row], columns[col - k - 1])
+                    left_moves = True
+                    bluefound = False
+                    print(bluefound)
+                    break                
+            if checkLeft(row, col - k ) == 0:
+                break
+            if checkLeft(row,col - k ) == 1:
+                break
+           
+        # if left_moves == True:
+        #     for g in range(left_moves):
+        #         board[rowOfRed,colOfRed - g-1] = 1
+        #         print('left_moves is true')
+        #         break
+        # redplayer(rows[row], columns[col])
+                 
+    print(board)
     
+    break
 
-    
-    
-    
-
-
-
-
-
-#if redTurn == True:
-#    blueTurn = True
-#    redTurn = False
-#else:
-#    blueTurn = True
-#    redTurn = False
+#wn.mainloop()
 
 
 
@@ -102,5 +156,3 @@ while redTurn:
 
 
 
-
-        
